5e355cc2d0633a4bbc3df7250f56a80b65e1817b
oasis1340 <oasis1340@gmail.com>
2025-03-02 11:55:17 +0900

    programmers

--- task/src/task/Solution.java
+++ task/src/task/Solution.java
@@ -2,23 +2,26 @@ package task;
 
 import java.util.Scanner;
 
-//2자리 이상의 정수 number가 주어집니다. 
-//주어진 코드는 이 수를 2자리씩 자른 뒤, 
-//자른 수를 모두 더해서 그 합을 출력하는 코드입니다. 
-//코드가 올바르게 작동하도록 한 줄을 수정해 주세요.
-
 public class Solution {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int number = sc.nextInt();
-        int answer = 0;
-        
-        while (number > 0) {
-            answer += number % 100;
-            number /= 100;
-        }
+        String code = sc.next();
+        String lastFourWords = code.substring(code.length()-4, code.length());
 
-        System.out.println(answer);
-        
+        if(lastFourWords.equals("_eye")){
+            System.out.println("Ophthalmologyc");
+        }
+        else if(lastFourWords.equals("head")){
+            System.out.println("Neurosurgery");
+        }
+        else if(lastFourWords.equals("infl")){
+            System.out.println("Orthopedics");
+        }
+        else if (lastFourWords.equals("skin")){
+            System.out.println("Dermatology");
+        }
+        else{
+            System.out.println("direct recommendation");
+        }
     }
 }
