0323db0b414d5b0d39c3e76857d7a7358d8bc9fc
oasis1340 <oasis1340@gmail.com>
2025-07-21 10:42:44 +0900

    백준 2743번

--- study/src/algorithm/Main.java
+++ study/src/algorithm/Main.java
@@ -3,41 +3,12 @@ package algorithm;
 import java.util.Scanner;
 
 public class Main {
-
+	
 	public static void main(String[] args) {
-		int dice1 = 0;
-		int dice2 = 0;
-		int dice3 = 0;
-		
-		int max = 0;
-		int total = 0;
-		
 		Scanner sc = new Scanner(System.in);
 		
-		dice1 = sc.nextInt();
-		dice2 = sc.nextInt();
-		dice3 = sc.nextInt();
+		String word = sc.next();
 		
-		
-		if (dice1 == dice2 && dice2 == dice3) {
-			total = 10000 + dice1 * 1000;
-			System.out.println(total);
-		}else if (dice1 == dice2 || dice1 == dice3) {
-			total = 1000 + dice1 * 100;
-			System.out.println(total);
-		}else if (dice2 == dice3) {
-			total = 1000 + dice2 * 100;
-			System.out.println(total);
-		}else {
-			max = dice1;
-			if (dice2 > max) {
-				max = dice2;
-			}
-			if (dice3 > max) {
-				max = dice3;
-			}
-			total = max * 100;
-			System.out.println(total);
-		}
+		System.out.println(word.length());
 	}
 }
