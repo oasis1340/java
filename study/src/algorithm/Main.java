851528a830a8c68a9837404523ff3eab0eccc729
oasis1340 <oasis1340@gmail.com>
2025-08-26 02:30:11 +0900

    백준 1546

--- study/src/algorithm/Main.java
+++ study/src/algorithm/Main.java
@@ -1,6 +1,6 @@
 package algorithm;
 
-import java.math.BigInteger;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
@@ -8,10 +8,35 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		BigInteger n = sc.nextBigInteger();
-        BigInteger m = sc.nextBigInteger();
+		ArrayList<Double> nums = new ArrayList<>();
 		
-		System.out.println(n.divide(m));
-		System.out.println(n.remainder(m));
+		int count = sc.nextInt();
+		int max = 0;
+		double avg = 0;
+		double sum = 0;
+		
+		for (int i = 0; i < count; i++) {
+			int num = sc.nextInt();
+			nums.add((double) num);
+			
+			if (num > max) {
+				max = num;
+			}
+		}
+		
+		for (int j = 0; j < count; j++) {
+			double num = nums.get(j);
+			nums.set(j, num / max * 100);
+		}
+		
+		
+		for (int k = 0; k < count; k++) {
+			double num = nums.get(k);
+			sum += num;
+		}
+		
+		avg = sum / count;
+		
+		System.out.println(avg);
 	}
 }
