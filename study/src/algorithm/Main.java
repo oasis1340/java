8f021fac3b9d5b08f5d001449602e33677a6aa45
oasis1340 <oasis1340@gmail.com>
2025-08-25 23:45:15 +0900

    백준 1271

--- study/src/algorithm/Main.java
+++ study/src/algorithm/Main.java
@@ -1,27 +1,17 @@
 package algorithm;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.math.BigInteger;
 import java.util.Scanner;
-import java.util.stream.Collectors;
 
 public class Main {
 	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		int num = 0;
+		BigInteger n = sc.nextBigInteger();
+        BigInteger m = sc.nextBigInteger();
 		
-		List<Integer> nums = new ArrayList<>();
-		
-		for (int i = 0; i < 10; i++) {
-			num = sc.nextInt();
-			int remainder = num % 42;
-			nums.add(remainder);
-		}
-		
-		List<Integer> remainders = nums.stream().distinct().collect(Collectors.toList());
-		
-		System.out.println(remainders.size());
+		System.out.println(n.divide(m));
+		System.out.println(n.remainder(m));
 	}
 }
