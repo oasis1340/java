b25a45211c57fd8c6f6e4a3ae962c044ad8d881b
oasis1340 <oasis1340@gmail.com>
2025-08-29 00:11:48 +0900

    백준 11718

--- study/src/algorithm/Main.java
+++ study/src/algorithm/Main.java
@@ -1,29 +1,14 @@
 package algorithm;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-	
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		String s = sc.next();
-		
-		int[] arr = new int[26];
-		Arrays.fill(arr, -1);
-		
-		for (int i = 0; i < s.length(); i++) {
-			char c = s.charAt(i);
-			
-			if (arr[c - 'a'] == -1) {
-				arr[c - 'a'] = i;
-			}
-		}
-		
-		for (int i = 0; i < arr.length; i++) {
-			System.out.print(arr[i] + " ");
-		}
-		
-	}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        while (sc.hasNextLine()) {
+            String text = sc.nextLine();
+            System.out.println(text);
+        }
+    }
 }
