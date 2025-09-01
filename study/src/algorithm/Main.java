06626efd08551cdd9843a77835ff0cd3b64e5ad7
oasis1340 <oasis1340@gmail.com>
2025-09-01 23:50:01 +0900

    백준 1157

--- study/src/algorithm/Main.java
+++ study/src/algorithm/Main.java
@@ -6,9 +6,25 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
-        while (sc.hasNextLine()) {
-            String text = sc.nextLine();
-            System.out.println(text);
+        String text = sc.next().toUpperCase();
+        
+        int max = -1;
+        char result = '0';
+        int[] count = new int[26];
+        
+        for (int i = 0; i < text.length(); i++) {
+        	count[text.charAt(i) - 'A']++;
         }
+        
+        for (int i = 0; i < 26; i++) {
+        	if (count[i] > max) {
+        		max = count[i];
+        		result = (char) (i + 'A');
+        	} else if (count[i] == max) {
+        		result = '?';
+        	}
+        }
+        
+        System.out.println(result);
     }
 }
