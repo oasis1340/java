919d980f4f59cdb0c3bf44bcc3a29fa32766fa60
oasis1340 <oasis1340@gmail.com>
2025-09-11 19:09:05 +0900

    백준 27323

--- study/src/algorithm/Main.java
+++ study/src/algorithm/Main.java
@@ -6,25 +6,12 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
-        String text = sc.next().toUpperCase();
+        int width = sc.nextInt();
+        int length = sc.nextInt();
         
-        int max = -1;
-        char result = '0';
-        int[] count = new int[26];
-        
-        for (int i = 0; i < text.length(); i++) {
-        	count[text.charAt(i) - 'A']++;
-        }
-        
-        for (int i = 0; i < 26; i++) {
-        	if (count[i] > max) {
-        		max = count[i];
-        		result = (char) (i + 'A');
-        	} else if (count[i] == max) {
-        		result = '?';
-        	}
-        }
+        int result = width * length;
         
         System.out.println(result);
+        
     }
 }
