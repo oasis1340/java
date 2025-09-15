4d40313a78aebf1fa655578ef08f0a8d7f7ec5ad
oasis1340 <oasis1340@gmail.com>
2025-09-15 22:42:35 +0900

    백준 5086

--- study/src/algorithm/Main.java
+++ study/src/algorithm/Main.java
@@ -1,15 +1,39 @@
 package algorithm;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+    	
         Scanner sc = new Scanner(System.in);
+                
+        String factor = "factor";
+        String multiple = "multiple";
+        String neither = "neither";
         
-        Long num = sc.nextLong();
+        ArrayList<String> result = new ArrayList<String>();
         
-        Long result = num * 4;
+		while ( true ) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			
+			if (a != 0 && b != 0) {
+				if ( a >= b && a % b == 0 ) {
+					result.add(multiple);
+				} else if ( a < b && b % a == 0 ) {
+					result.add(factor);
+				} else if ( a % b != 0 || b % a != 0 ) {
+					result.add(neither);
+				}
+			}
+			if ( a == 0 && b == 0) {
+				break;
+			}
+		}
         
-        System.out.println(result);    
+        for (int i = 0; i < result.size(); i++) {
+        	System.out.println(result.get(i));
+        }
     }
 }
