8d8d8e2e564193020b64f5eb63cea588c087491d
oasis1340 <oasis1340@gmail.com>
2025-09-17 17:10:21 +0900

    백준 2750

--- study/src/algorithm/Main.java
+++ study/src/algorithm/Main.java
@@ -9,24 +9,19 @@ public class Main {
     	Scanner sc = new Scanner(System.in);
     	
     	int num = 0;
-    	int sum = 0;
-    	int avg = 0;
-    	int mid = 0;
+    	int count = sc.nextInt();
     	
     	ArrayList<Integer> nums = new ArrayList<Integer>();
     	
-    	for (int i = 0; i < 5; i++) {
+    	for (int i = 0; i < count; i++) {
     		num = sc.nextInt();
     		nums.add(num);
-    		sum += num;
     	}
     	
     	Collections.sort(nums);
     	
-    	avg = sum / 5;
-    	mid = nums.get(2);
-    	
-    	System.out.println(avg);
-    	System.out.println(mid);
+    	for (int i = 0; i < nums.size(); i++) {
+    		System.out.println(nums.get(i));
+    	}
     }    
 }
