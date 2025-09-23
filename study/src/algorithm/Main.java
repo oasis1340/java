31f04f04239b8b856c2cf43eeef8974f33f9aeaf
oasis1340 <oasis1340@gmail.com>
2025-09-23 16:45:31 +0900

    백준 2675

--- study/src/algorithm/Main.java
+++ study/src/algorithm/Main.java
@@ -1,27 +1,30 @@
 package algorithm;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        String n = sc.next();
-        
-        ArrayList<Integer> nums = new ArrayList<>();
+        int count = sc.nextInt();
+        ArrayList<String> print = new ArrayList<String>();
 
-        for (int i = 0; i < n.length(); i++) {
-            String[] num = (n.split(""));
-            nums.add(Integer.parseInt(num[i]));
+        for (int i = 0; i < count; i++) {
+        	int repeat = sc.nextInt();
+        	String text = sc.next();
+        	
+        	String result = "";
+        	for (int j = 0; j < text.length(); j++) {
+        		for (int k = 0; k < repeat; k++) {
+        			result += text.charAt(j);
+        		}
+        	}
+        	print.add(result);
         }
-
-        Collections.sort(nums);
-        Collections.reverse(nums);
         
-        for (int i = 0; i < nums.size(); i++) {
-        	System.out.print(nums.get(i));
+        for (int i = 0; i < print.size(); i++) {
+        	System.out.println(print.get(i));
         }
     }
 }
