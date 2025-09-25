bae4df4ddafa0b4d6f138e9547ce66fc117f541a
oasis1340 <oasis1340@gmail.com>
2025-09-26 00:02:47 +0900

    백준 11050

--- study/src/algorithm/Main.java
+++ study/src/algorithm/Main.java
@@ -1,30 +1,44 @@
 package algorithm;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int count = sc.nextInt();
-        ArrayList<String> print = new ArrayList<String>();
-
-        for (int i = 0; i < count; i++) {
-        	int repeat = sc.nextInt();
-        	String text = sc.next();
-        	
-        	String result = "";
-        	for (int j = 0; j < text.length(); j++) {
-        		for (int k = 0; k < repeat; k++) {
-        			result += text.charAt(j);
-        		}
-        	}
-        	print.add(result);
+        int n = sc.nextInt();
+        int nCount = n;
+        int k = sc.nextInt();
+        int kCount = k;
+        int df = n - k;
+        int dfCount = df;
+        int nF = 1;
+        int kF = 1;
+        int dF = 1;
+        
+        // n! / (k!(n-k)!)
+        
+        for (int i = 1; i < nCount; i++) {
+        	nF *= n;
+        	n--;
         }
         
-        for (int i = 0; i < print.size(); i++) {
-        	System.out.println(print.get(i));
+        System.out.println(nF);
+        
+        for (int i = 1; i < kCount; i++) {
+        	kF *= k;
+        	k--;
         }
+        
+        System.out.println(kF);
+        
+        for (int i = 1; i < dfCount; i++) {
+        	dF *= df;
+        	df--;
+        }
+        
+        System.out.println(dF);
+        
+        System.out.println(nF/(kF*dF));
     }
 }
